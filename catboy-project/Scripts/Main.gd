16a9141b8926652d7ea8e4357986fd8045fc6caf
Warspiteful <jalmendral24@gmail.com>
2021-10-22 08:12:41 -0400

    Created Colored Text to track index using bbCode

--- catboy-project/Scripts/Main.gd
+++ catboy-project/Scripts/Main.gd
@@ -10,11 +10,12 @@ func find_new_active_enemy(typed_character: String):
 	for enemy in enemy_container.get_children():
 		var prompt = enemy.get_prompt()
 		var next_char = prompt.substr(0,1)
-
+	
 		if next_char == typed_character:
 			active_enemy = enemy
-			print("Found New Enemy that starts with %s" %  next_char)
 			current_letter_index = 1
+			active_enemy.set_next_character(current_letter_index)
+			return
 	
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey and not event.is_pressed():
@@ -31,6 +32,7 @@ func _input(event: InputEvent) -> void:
 			if key_typed == next_char:
 				print("successfully typed %s " % key_typed)
 				current_letter_index += 1
+				active_enemy.set_next_character(current_letter_index)
 				if current_letter_index == prompt.length()-1:
 					print("Killed Enemy!")
 					current_letter_index = -1
